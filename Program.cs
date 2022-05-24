1ac25e42d3df7baf219cdacbd12e74adfece5c83
Daniel Brandão <daniel.brdao@gmail.com>
2022-05-24 19:26:24 -0300

    inclusão de uma aplicação console no projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int[] array = { 1, 2, 3, 4, 5 };
+int resto =0;
+int total = 0;
+
+foreach(var valor in array)
+{
+    resto = valor % 2;
+    if(resto == 0)
+    {
+        Console.WriteLine(valor);
+        total += valor;
+    }
+}
+
+Console.WriteLine($"Total: {total}");
